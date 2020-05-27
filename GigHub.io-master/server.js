9436ed7c05ee159420e817fc16624887858bb801
tbiehl22 <57846653+tbiehl22@users.noreply.github.com>
2020-05-26 21:39:39 -0600

    updated database name

--- GigHub.io-master/server.js
+++ GigHub.io-master/server.js
@@ -34,7 +34,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB, and allow Heroku to connect to provisioned db
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/gighub");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/dreamjob");
 
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
